7c7890ee61471b446366753d22d7a7141a0d8b07
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-04-23 13:16:41 +0530

    routes post to get for profile

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const checkAuth = require("../middlewares/check-auth")
 const ProfileController = require("../controllers/profile")
 
-router.post("/view/:userId", ProfileController.profile_view)
+router.get("/view/:userId", ProfileController.profile_view)
 router.post("/view_self", checkAuth, ProfileController.profile_view_self)
 router.post("/update", checkAuth, ProfileController.profile_update)
 
