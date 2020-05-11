54270bfc28158366e3b0f6411d7a652d85f6ea32
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-05-11 16:29:39 +0530

    password

--- api/models/user.js
+++ api/models/user.js
@@ -15,7 +15,6 @@ const userSchema = mongoose.Schema({
     match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
   },
   password: { type: String ,
-    required: true,
   },
   dob: { type: Number },
   description: { type: String,
