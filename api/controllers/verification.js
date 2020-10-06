1e411491d00600ffb2bd38f8ec544cce247d31a0
Gaurav <gauravrai568@gmail.com>
2020-10-06 23:33:28 +0530

    added photo upload for verification form

--- api/controllers/verification.js
+++ api/controllers/verification.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const cloudinary = require('./cloudinary')
 const User = require('../models/user')
 const VerificationForm = require('../models/verificationForm')
 const { use } = require('passport')
